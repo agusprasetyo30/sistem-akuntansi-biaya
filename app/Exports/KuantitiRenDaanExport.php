f18bff73eefbdacc53966d2fd4a9756548ac9533
pilotjinix <agungsantoso557@gmail.com>
2022-12-22 09:11:37 +0700

    Kuantiti Rencana Pengadaan import done

---
+++ app/Exports/KuantitiRenDaanExport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Asumsi_Umum;
+use App\Models\QtyRenDaan;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class KuantitiRenDaanExport implements WithHeadings
+{
+
+    protected $version;
+
+    function __construct($version)
+    {
+        $this->version = $version;
+    }
+
+
+    public function headings(): array
+    {
+        try {
+            $template = ['material_code', 'region_id'];
+            $data = Asumsi_Umum::where('version_id', $this->version)
+                ->get();
+
+            foreach ($data as $items){
+                $temp = format_month($items->month_year, 'fe').'|'.$items->id;
+                array_push($template, $temp);
+            }
+
+            return $template;
+        }catch (\Exception $exception){
+            return $template;
+        }
+    }
+}
