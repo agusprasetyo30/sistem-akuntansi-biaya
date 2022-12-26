9ace4790a87c8e9fe57b845d618599e2c1b8cee0
pilotjinix <agungsantoso557@gmail.com>
2022-12-26 15:33:39 +0700

    Price Rencana Pengadaan import modify

---
+++ app/Exports/Template/T_PriceRenDaanExport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Exports\Template;
+
+use App\Models\Asumsi_Umum;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithTitle;
+
+class T_PriceRenDaanExport implements WithHeadings, WithTitle
+{
+    protected $version;
+
+    function __construct($version)
+    {
+        $this->version = $version;
+    }
+
+    public function title(): string
+    {
+        return 'Template Kuantiti Rencana Pengadaan';
+    }
+
+    public function headings(): array
+    {
+        try {
+            $template = ['material_code', 'region_id'];
+            $data = Asumsi_Umum::where('version_id', $this->version)
+                ->get();
+
+            foreach ($data as $items) {
+                $temp = format_month($items->month_year, 'ye') . ' | ' . $items->id;
+                array_push($template, $temp);
+            }
+            return $template;
+        } catch (\Exception $exception) {
+            return $template;
+        }
+    }
+}
