75f5ca04f45442bbdb20101dd8ea40e026ee7bde
Bayu Luky <lukybayu@gmail.com>
2023-03-02 12:17:55 +0700

    zco : update formula cr

--- app/DataTables/Master/H_ZcoDataTable.php
+++ app/DataTables/Master/H_ZcoDataTable.php
@@ -157,7 +157,7 @@ class H_ZcoDataTable extends DataTable
                 }
 
                 $cr = 0;
-                if ($total_qty->total_qty > 0 && $tot_kuanprod > 0) {
+                if ($total_qty->total_qty != 0 && $tot_kuanprod > 0) {
                     $cr = $total_qty->total_qty / $tot_kuanprod;
                 }
 
@@ -227,7 +227,7 @@ class H_ZcoDataTable extends DataTable
                 }
 
                 $cr = 0;
-                if ($total_qty->total_qty > 0 && $tot_kuanprod > 0) {
+                if ($total_qty->total_qty != 0 && $tot_kuanprod > 0) {
                     $cr = $total_qty->total_qty / $tot_kuanprod;
                 }
 
