cd9b210ac8387b89e6ddba694b831c9b70d5701d
pilotjinix <agungsantoso557@gmail.com>
2023-03-06 08:10:37 +0700

    Fixing To GCP

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
-    protected $table = 'roles';
+    protected $table = 'role';
     protected $primaryKey = 'id';
 
     protected $fillable = [
