fe6537c52e4c257b9acd8996ae003e788fc4917d
pilotjinix <agungsantoso557@gmail.com>
2023-03-10 08:46:40 +0700

    Management Role

--- resources/views/pages/master/management_role/add.blade.php
+++ resources/views/pages/master/management_role/add.blade.php
@@ -1,6 +1,6 @@
 <!-- Modal -->
 <div class="modal fade" id="modal_add" tabindex="-1" role="dialog" data-bs-backdrop="static" data-bs-keyboard="false" aria-labelledby="largemodal" aria-hidden="true">
-    <div class="modal-dialog modal-lg " role="document">
+    <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="largemodal1">Tambah Management Role</h5>
