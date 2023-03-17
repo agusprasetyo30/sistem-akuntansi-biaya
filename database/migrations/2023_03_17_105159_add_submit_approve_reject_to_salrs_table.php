d01b12611af522de3dddf383ffd0733e654a654f
bayuluky <lukybayu@gmail.com>
2023-03-17 13:31:56 +0700

    seeder : add seeder for submit, approve, reject and fix bug cons rate

---
+++ database/migrations/2023_03_17_105159_add_submit_approve_reject_to_salrs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSubmitApproveRejectToSalrsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('salrs', function (Blueprint $table) {
+            $table->dateTime('submited_at')->nullable();
+            $table->integer('submited_by')->nullable();
+            $table->dateTime('approved_at')->nullable();
+            $table->integer('approved_by')->nullable();
+            $table->dateTime('rejected_at')->nullable();
+            $table->integer('rejected_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('salrs', function (Blueprint $table) {
+            //
+        });
+    }
+}
--- database/seeders/ManagementRoleSeeder.php
+++ database/seeders/ManagementRoleSeeder.php
@@ -17,36 +17,353 @@ class ManagementRoleSeeder extends Seeder
         Management_Role::create([
             'role_id' => 1,
             'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'role',
         ]);
 
         Management_Role::create([
             'role_id' => 1,
             'company_code' => 'B000',
-        ]);
-
-        Management_Role::create([
-            'role_id' => 2,
-            'company_code' => 'B000',
-        ]);
-
-        Management_Role::create([
-            'role_id' => 3,
-            'company_code' => 'B000',
-        ]);
-
-        Management_Role::create([
-            'role_id' => 4,
-            'company_code' => 'B000',
-        ]);
-
-        Management_Role::create([
-            'role_id' => 5,
-            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'users',
         ]);
 
         Management_Role::create([
             'role_id' => 1,
             'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'mapping_role',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'management_role',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'company',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'kategori_material',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'kategori_produk',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'kategori_balans',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'map_kategori_balans',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'group_account',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'gl_account',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'group_account_fc',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'gl_account_fc',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'cost_center',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'material',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'plant',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'glos_cc',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'tarif',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'asumsi_umum',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'kurs',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'regions',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'cons_rate',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'saldo_awal',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'qty_renprod',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'qty_rendaan',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'price_rendaan',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'zco',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'salrs',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'laba_rugi',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'pj_pemakaian',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'pj_penjualan',
+        ]);
+
+        Management_Role::create([
+            'role_id' => 1,
+            'company_code' => 'B000',
+            'create' => true,
+            'read' => true,
+            'update' => true,
+            'delete' => true,
+            'approve' => true,
+            'db' => 'balans',
         ]);
     }
 }
